58c39bba02449e44336a32f200fb566b0601512d
Kyle Cordes <kyle.cordes@oasisdigital.com>
2018-03-09 23:00:43 -0600

    curriculum update

--- src/app/company-loader.service.ts
+++ src/app/company-loader.service.ts
@@ -4,12 +4,6 @@ import { map } from 'rxjs/operators';
 
 const API_URL = 'https://api.angularbootcamp.com';
 
-// Or connect directly to the API server (local development only)
-// const API_URL = 'http://localhost:8085';
-
-// Or connect to the hosted demo API (works from local, Cloud 9, StackBlitz):
-// const API_URL = 'https://api.angularbootcamp.com';
-
 @Injectable()
 export class CompanyLoader {
   constructor(private http: HttpClient) { }
