69da64f3738767ca799c21d3456aa72c3d50c9a5
Kyle Cordes <kyle.cordes@oasisdigital.com>
2018-04-03 11:11:13 -0500

    curriculum update

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import { Observable } from 'rxjs/Observable';
+import { Observable } from 'rxjs';
 import { publishReplay, refCount } from 'rxjs/operators';
 
 import { CompanyLoader } from './company-loader.service';
