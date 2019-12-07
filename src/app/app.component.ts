5dda34b19ba34c9d12dcbc69625f91856ee688b8
Zachary Kipping <zachary.kipping@oasisdigital.com>
2019-12-06 21:53:28 -0600

    curriculum update

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,15 +2,15 @@ import { Component } from '@angular/core';
 import { Observable } from 'rxjs';
 import { shareReplay } from 'rxjs/operators';
 
-import { CompanyLoader } from './company-loader.service';
+import { Company, CompanyLoader } from './company-loader.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html'
 })
 export class AppComponent {
-  company: Observable<any>;
-  flag = false;
+  company: Observable<Company>;
+  showAgain = false;
 
   constructor(loader: CompanyLoader) {
     this.company = loader.loadOneCompany().pipe(shareReplay(1));
